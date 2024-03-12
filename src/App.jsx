ea9f95836e47765eda323a97fac168f79302678e
tasib94 <rahi84974@gmail.com>
2024-03-13 01:36:40 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1>React core concepts</h1>
+      <Device name="laptop" price="25500tk"></Device>
+      <Device name="mobile" price="16000tk"></Device>
+      <Device name="lens" price="1300tk"></Device>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Developer></Developer>
+
+
+    </>
+  )
+}
+
+function Device(props){
+  return <h3>Device name: {props.name} and price: {props.price}</h3>
+}
+
+function Person(){
+  const age = 23;
+  const person = {name : 'Tasib'};
+  return <h3 className='student'>I am a {person.name} with age: {age}</h3>
+}
+
+function Developer(){
+  const developerStyle = {
+    margin:'20px',
+    padding:'20px',
+    border:'2px solid steelblue',
+    borderRadius:'10px'
+  }
+
+  return(
+    <div style={developerStyle}>
+      <h4> Hello developers!!</h4>
+      <p>Coding</p>
+    </div>
+  )
+}
+
+export default App
